2abae477e9c545aa2105b99ea5053e963b553d65
Kanchana224 <kkanchana427@gmail.com>
2023-10-27 19:59:34 +0530

    updated

--- script.js
+++ script.js
@@ -7,15 +7,15 @@ var result1=JSON.stringify(obj2)
 console.log(result===result1) 
 
 
-var request=new XMLHttpRequest() 
-request.open("GET","https://restcountries.com/v3.1/all","true") 
-request.send(); 
-request.onload=function(){
-var data=request.response;
-var result=JSON.parse(data)
-console.log(result) 
-for(var i=0;i<result.length;i++){
-console.log(result[i].flags.png)}
+var request1=new XMLHttpRequest() 
+request1.open("GET","https://restcountries.com/v3.1/all","true") 
+request1.send(); 
+request1.onload=function(){
+var data1=request1.response;
+var result1=JSON.parse(data1)
+console.log(result1) 
+for(var i=0;i<result1.length;i++){
+console.log(result1[i].flags.png)}
 } 
 
 
