ded8dad1f3581fa9c52e82905d4b705445cf957e
Kanchana224 <kkanchana427@gmail.com>
2023-10-27 16:10:25 +0530

    update successful

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// Question1
-//        How to compare two JSON the same properties without order 
+
        obj1={name:"person 1",age:5};
        obj2={age:5,name:"person 1"};
 
@@ -7,9 +6,6 @@ var result=JSON.stringify(obj1)
 var result1=JSON.stringify(obj2)
 console.log(result===result1) 
 
-// Question2
-//        Use the rest countries API URL=> https://restcountries.com/v3.1/all and display all the country
-//        flags in the console.
 
 var request=new XMLHttpRequest() 
 request.open("GET","https://restcountries.com/v3.1/all","true") 
@@ -22,9 +18,6 @@ for(var i=0;i<result.length;i++){
 console.log(result[i].flags.png)}
 } 
 
-// Question3
-//           Use the rest countries API URL=> https://restcountries.com/v3.1/all and display all the country
-//           names,regions,subregions,population in the console.   
 
 var request=new XMLHttpRequest() 
 request.open("GET","https://restcountries.com/v3.1/all","true") 
